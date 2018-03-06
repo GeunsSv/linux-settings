c143465d37ea2c92a6d201deb76af6345b06e424
Sven Geuns <sgeuns@nalys-group.com>
2018-03-06 11:32:54 +0100

    removed adding of git wip to git repository and moved it to bash_aliases (glo) and added comments

--- settings.sh
+++ settings.sh
@@ -7,39 +7,35 @@ fi
 exec > ~/scripts/logs/degug-linux-settings.log 2>&1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# changes for git repositories
-if [ "$1" == "git" ]
-then
-        echo "[alias]" >> $2/.git/config
-        echo "        wip = log --abbrev-commit --oneline --graph --decorate" >> $2/.git/config
+# Custumize linux to my personal taste
+
+## Insert neccesary info for spotify-client
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+        --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | \
+        sudo tee /etc/apt/sources.list.d/spotify.list
+
+## Update and upgrade, install programs
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt install -y wmctrl terminator thunderbird spotify-client sl
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+## Add docker info to repositories and install it
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt install -y docker-ce
+
+## Make sure the autostart folder exists for the startup script
+if [[ ! -d ~/.config/autostart ]]; then
+        mkdir ~/.config/autostart/
 fi
 
-# custumize linux to my personal taste
-if [ "$1" == "linux" ]
-then
-        # Insert neccesary info for spotify-client
-        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
-                --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-        echo deb http://repository.spotify.com stable non-free | \
-                sudo tee /etc/apt/sources.list.d/spotify.list
+## Copy all files
+cp $DIR/files/bash_aliases ~/.bash_aliases
+cp $DIR/files/vimrc ~/.vimrc
+cp $DIR/files/startup.sh ~/scripts/
+cp $DIR/files/close_all_windows.sh ~/scripts/
+cp $DIR/files/sh.desktop ~/.config/autostart/
 
-        sudo apt-get update && sudo apt-get -y upgrade
-        sudo apt install -y wmctrl terminator thunderbird spotify-client sl
-        sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-        sudo apt-get update
-        sudo apt install -y docker-ce
-
-        if [[ ! -d ~/.config/autostart ]]; then
-                mkdir ~/.config/autostart/
-        fi
-
-        cp $DIR/files/bash_aliases ~/.bash_aliases
-        cp $DIR/files/vimrc ~/.vimrc
-        cp $DIR/files/startup.sh ~/scripts/
-        cp $DIR/files/close_all_windows.sh ~/scripts/
-        cp $DIR/files/sh.desktop ~/.config/autostart/
-
-        . ~/.bashrc
-fi
+. ~/.bashrc
