e2f3a265d7daa6fb3fda15d57d1d9ef86e0b5d21
Sven Geuns <sgeuns@nalys-group.com>
2018-03-24 15:45:46 +0100

    fixed typo

--- files/close_all_windows.sh
+++ files/close_all_windows.sh
@@ -7,7 +7,7 @@ exec > $HOME/scripts/logs/close-all-windows.log 2>&1
 echo "This log file should be empty!" > $HOME/scripts/logs/close-all-windows.log
 
 # close all open windows gracefully without closing the Desktop environment
-WIN_IDs=$(wmctrl -l | grep -vwE "Desktop$|xfce4-panel$|$USER$" | cut -f1 -d' ')
+WIN_IDs=$(wmctrl -l | grep -vwE "Desktop$|xfce4-panel$|$USER" | cut -f1 -d' ')
 for i in $WIN_IDs; do 
         wmctrl -ic "$i"
 done
@@ -15,5 +15,5 @@ done
 # Keep checking and waiting until all windows are closed (you probably don't need this section)
 while test $WIN_IDs; do 
         sleep 0.1; 
-        WIN_IDs=$(wmctrl -l | grep -vwE "Desktop$|xfce4-panel$|$USER$" | cut -f1 -d' ')
+        WIN_IDs=$(wmctrl -l | grep -vwE "Desktop$|xfce4-panel$|$USER" | cut -f1 -d' ')
 done 
